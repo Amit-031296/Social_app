3fbfe038af943c45666975b70f0b39bea13901e3
Amit-031296 <amitv.mumbaii@gmail.com>
2021-08-03 16:23:46 +0530

    models 3-8-2021

--- social-network-app/src/profiles/models.py
+++ social-network-app/src/profiles/models.py
@@ -33,6 +33,7 @@ class ProfileManager(models.Manager):
     def get_all_profiles(self, me):
         # profiles = Profile.objects.all().exclude(user=me)
         print("profiles")
+        print("profiles in models")
         a = Profile.objects.get(user=me)
         b = a.blocked_user.all()
         list1 = list()
